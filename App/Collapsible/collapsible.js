82dc589cd8e8399f2b98cd39c23f64115d27a472
Naman-Rudrakshi <142565166+Naman-Rudrakshi@users.noreply.github.com>
2023-08-28 23:50:04 -0700

    Delete App directory

--- App/Collapsible/collapsible.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.collapsible-button');
-const section = document.querySelector('.collapsible-section');
-
-button.addEventListener('click', () => {
-  section.classList.toggle('show');
-});
