99c181c2af32a6fe86bbbd8fd04ce256d4d1a4af
Jay9971 <65046207+Jay9971@users.noreply.github.com>
2023-09-02 23:52:41 -0700

    Delete VTBuilder directory

--- VTBuilder/src/main/java/com/jay9971/VTBuilder/Repository/ArchiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jay9971.VTBuilder.Repository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArchiveRepository extends JpaRepository<Archive, Long> {
-	
-}
