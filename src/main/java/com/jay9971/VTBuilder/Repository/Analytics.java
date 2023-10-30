efa6bd525006cdf797d6acf3257087f875271034
Naman-Rudrakshi <142565166+Naman-Rudrakshi@users.noreply.github.com>
2023-10-30 02:22:34 -0700

    Add files via upload

---
+++ src/main/java/com/jay9971/VTBuilder/Repository/Analytics.java
@@ -0,0 +1,74 @@
+package com.jay9971.VTBuilder.Repository;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Analytics {
+	private int numOfPlayers;
+	
+	private String timeElapsed;
+	
+	private String teamScore;
+	
+	@Id
+	private long id;
+	
+	public Analytics() {
+		
+	}
+	
+	public Analytics(long id) {
+		this.id = id;
+		
+	}
+	
+	public Analytics(long id, int numOfPlayers, String timeElapsed, String teamScore) {
+		this.id = id;
+		this.numOfPlayers = numOfPlayers;
+		this.timeElapsed = timeElapsed;
+		this.teamScore = teamScore;
+	}
+
+	public int getNumOfPlayers() {
+		return numOfPlayers;
+	}
+
+	public void setNumOfPlayers(int numOfPlayers) {
+		this.numOfPlayers = numOfPlayers;
+	}
+
+	public String getTimeElapsed() {
+		return timeElapsed;
+	}
+
+	public void setTimeElapsed(String timeElapsed) {
+		this.timeElapsed = timeElapsed;
+	}
+
+	public String getTeamScore() {
+		return teamScore;
+	}
+
+	public void setTeamScore(String teamScore) {
+		this.teamScore = teamScore;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Analytics [numOfPlayers=" + numOfPlayers + ", timeElapsed=" + timeElapsed + ", teamScore=" + teamScore
+				+ ", id=" + id + "]";
+	}
+
+	
+	
+	
+}
